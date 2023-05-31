3ac6f5592029c58bf1acee97335d47502077be9a
Jens Winter-Hübenthal <jens.winter@gmail.com>
2023-05-31 20:49:05 +0200

    next

--- src/dwd-harvester/index.js
+++ src/dwd-harvester/index.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const os = require('os');
 const path = require('path');
 const { execSync } = require('child_process');
+const gdal = require('gdal-async');
 
 
 const pgServer = 'localhost';
@@ -29,18 +30,74 @@ async function fetchLastDate(client) {
 
     await client.connect();
 
+/*
     const lastDate = await fetchLastDate(client);
     console.log(`Last harvest: ${lastDate}`);
+
+    const endDate = new Date();
+    endDate.setDate(endDate.getDate() - 1);
+    endDate.setHours(0, 0, 0, 0);
+    console.log(`End date: ${endDate}`);
+
+    const dates = [];
+    const date = new Date(lastDate);
+    date.setHours(0, 0, 0, 0);
+    while (date <= endDate) {
+        dates.push(new Date(date));
+        date.setDate(date.getDate() + 1);
+    }
+    console.log(dates);
+
+    const downloadFiles = dates.map(date => {
+        const year = date.getFullYear();
+        const month = date.getMonth() + 1;
+        const day = date.getDate();
+        return `RW-${year}${month < 10 ? '0' : ''}${month}${day < 10 ? '0' : ''}${day}.tar.gz`;
+    }).join(',')
+    console.log(downloadFiles);
 
     const tmpDir = fs.mkdtempSync(path.join(os.tmpdir(), 'radolan-'));
     console.log(`Created temporary directory: ${tmpDir}`);
 
-    // Download radolan data from DWD since last harvest
-    const downloadCmd = `wget -r -np -nH -e robots=off -w 5 --random-wait -P ${tmpDir} -R index.html* -A 'RW-20230111.tar.gz' https://opendata.dwd.de/climate_environment/CDC/grids_germany/hourly/radolan/recent/asc/`;
+    const downloadCmd = `wget -r -np -nH -e robots=off -w 3 --random-wait -P ${tmpDir} -nd -R index.html* -A '${downloadFiles}' https://opendata.dwd.de/climate_environment/CDC/grids_germany/hourly/radolan/recent/asc/`;
     console.log(`Downloading data from DWD: ${downloadCmd}`);
+    execSync(downloadCmd);
 
-    const x = execSync(downloadCmd);
-    console.log(x.toString());
+    const uncompressCmd = `find ${tmpDir} -name '*.tar.gz' -exec tar -xzf {} -C ${tmpDir} \\;`;
+    console.log(`Uncompressing data: ${uncompressCmd}`);
+    execSync(uncompressCmd);
+*/
+
+
+    const dir = '/var/folders/5z/4vcjl51s5zzg05j1q4x9bwb40000gn/T/radolan-srqMjR';
+    const radolanDataFilenames = fs.readdirSync(dir).filter(file => file.endsWith('.asc'));
+
+    let counter = 0;
+    for (const filename of radolanDataFilenames) {
+        counter++;
+        console.log(`Processing: ${counter}/${radolanDataFilenames.length}`);
+
+        const filepath = path.join(dir, filename);
+        const outfile = path.join(dir, filename.replace('.asc', '.tif'));
+        const gdalwarpCmd = `gdalwarp ${filepath} ${outfile} -s_srs '+proj=stere +lon_0=10.0 +lat_0=90.0 +lat_ts=60.0 +a=6370040 +b=6370040 +units=m' -t_srs '+proj=stere +lon_0=10.0 +lat_0=90.0 +lat_ts=60.0 +a=6370040 +b=6370040 +units=m' -r near -of GTiff -cutline ./src/dwd-harvester/buffer.shp`;
+        console.log(`Executing: ${gdalwarpCmd}`);
+        execSync(gdalwarpCmd);
+
+        const dataset = await gdal.openAsync(outfile);
+        console.log(dataset);
+
+        const dst = await gdal.openAsync(dir, 'w', 'ESRI Shapefile', 900, 900);
+        console.log(dst);
+        const lyr = dst.layers.create(filename.replace('.tif', '.shp'), null, gdal.Polygon);
+        lyr.fields.add(new gdal.FieldDefn('MYFLD', gdal.OFTInteger))
+
+        gdal.polygonize({
+            src: dataset.bands.get(1),
+            dst: lyr,
+            pixValField: 11,
+            progress_cb: (complete, message) => console.log(`${complete}% ${message}`)
+        });
+    }
 
     await client.end();
 
